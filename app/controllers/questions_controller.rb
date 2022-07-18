c10ff4172ac125844ef74adb54ff7d213744e733
Archi1881 <detr1881@gmail.com>
2022-07-18 23:24:00 +0500

    refactoring before_action

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,4 @@
 class QuestionsController < ApplicationController
-  
-  before_action :load_question, only: [:show, :edit, :update]
-
   def index
     @questions = Question.all
   end
@@ -10,7 +7,6 @@ class QuestionsController < ApplicationController
   end
 
   def new
-    @question = Question.new
   end
 
   def edit
@@ -18,7 +14,7 @@ class QuestionsController < ApplicationController
 
   def create
     @question = Question.new(question_params)
-    
+
     if @question.save
       redirect_to @question
     else
@@ -27,18 +23,25 @@ class QuestionsController < ApplicationController
   end
 
   def update
-    if @question.update(question_params)
+    if question.update(question_params)
       redirect_to @question
     else
       render :edit
     end
   end
 
+  def destroy
+    question.destroy
+    redirect_to questions_path
+  end
+
   private
 
-  def load_question
-    @question = Question.find(params[:id])
+  def question
+    @question ||= params[:id] ? Question.find(params[:id]) : Question.new
   end
+
+  helper_method :question
 
   def question_params
     params.require(:question).permit(:title, :body)
--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -2,11 +2,12 @@ require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
   let(:question) { create(:question) }
-  
-  describe 'GET #index'do
+
+  describe 'GET #index' do
     let(:questions) { create_list(:question, 3) }
 
     before { get :index }
+
 
     it 'populates an array of all questions' do
       expect(assigns(:questions)).to match_array(questions)
@@ -18,12 +19,7 @@ RSpec.describe QuestionsController, type: :controller do
   end
 
   describe 'GET #show' do
-
     before { get :show, params: { id: question } }
-
-    it 'assigns the requested question to @question' do
-      expect(assigns(:question)).to eq question
-    end
 
 
     it 'renders show view' do
@@ -34,22 +30,14 @@ RSpec.describe QuestionsController, type: :controller do
   describe 'GET #new' do
     before { get :new }
 
-    it 'assigns a new Question to @question' do
-      expect(assigns(:question)).to be_a_new(Question)
-    end
-
     it 'renders new view' do
       expect(response).to render_template :new
     end
   end
 
   describe 'GET #edit' do
-
     before { get :edit, params: { id: question } }
 
-    it 'assigns the requested question to @question' do
-      expect(assigns(:question)).to eq question
-    end
 
     it 'renders edit view' do
       expect(response).to render_template :edit
@@ -72,6 +60,7 @@ RSpec.describe QuestionsController, type: :controller do
       it 'does not save the question' do
         expect { post :create, params: { question: attributes_for(:question, :invalid) } }.to_not change(Question, :count)
       end
+
 
       it 're-renders new view' do
         post :create, params: { question: attributes_for(:question, :invalid) }
@@ -116,5 +105,18 @@ RSpec.describe QuestionsController, type: :controller do
       end
     end
   end
-  
+
+  describe 'DELETE #destroy' do
+    let!(:question) { create(:question) }
+
+    it 'deletes the question' do
+      expect { delete :destroy, params: { id: question } }.to change(Question, :count).by(-1)
+    end
+
+    it 'redirects to index' do
+      delete :destroy, params: { id: question }
+      expect(response).to redirect_to questions_path
+    end
+  end
+
 end

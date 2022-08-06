7b861363086650df01a84c043152b6d5870ac0ae
Archi1881 <detr1881@gmail.com>
2022-08-07 01:51:43 +0500

    урок 6

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_31_122127) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_06_195417) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_31_122127) do
     t.datetime "updated_at", null: false
     t.bigint "question_id"
     t.bigint "user_id"
+    t.boolean "best", default: false
     t.index ["question_id"], name: "index_answers_on_question_id"
     t.index ["user_id"], name: "index_answers_on_user_id"
   end
